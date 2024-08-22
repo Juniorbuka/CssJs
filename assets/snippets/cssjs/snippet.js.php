0062de6f34c735d3810432f15910ec6f9e66aa25
Juniorbuka <e.volobuev@gmail.com>
2024-08-22 11:19:06 +0300

    Update snippet.js.php

--- assets/snippets/cssjs/snippet.js.php
+++ assets/snippets/cssjs/snippet.js.php
@@ -1,30 +1,31 @@
 <?php
 if(!defined('MODX_BASE_PATH')) {die('What are you doing? Get out of here!');}
 
-//параметры
-$files = isset($files) ? $files : ''; // Список файлов (css, scss, less)
-$minify = isset($minify) ? $minify : '1'; //сжымать и минифицировать файлы
-$folder = isset($folder) ? $folder : ''; // папка для сгенерированных стилей по умолчанию в корень
-//$inline = isset($inline) ? $inline : ''; // инлайн код стилей
-//$parse = isset($parse) ? $parse : '0'; //обрабатывать ли теги MODX
+// Parameters
+$files = isset($files) ? $files : ''; // List of files (css, scss, less)
+$minify = isset($minify) ? $minify : '1'; // Minify and compress files
+$folder = isset($folder) ? $folder : ''; // Folder for generated styles, default is the root
+$filename = isset($filename) ? $filename : 'scripts'; // Filename
+$filepre = isset($filepre) ? $filepre : ''; // Load file, e.g., (defer)
 
-//Обрабатываем файлы, преобразовываем less и scss
+// Process files, convert less and scss
 $filesArr = explode(',', str_replace('\n', '', $files));
 foreach ($filesArr as $key => $value) {
-	$file = MODX_BASE_PATH . trim($value);
-	$v[$key] =  filemtime($file);
-	$filesForMin[$key] = $file;  
+    $file = MODX_BASE_PATH . trim($value);
+    $v[$key] = filemtime($file);
+    $filesForMin[$key] = $file;
 }
+
 if ($minify == '1') {
-	include_once(MODX_BASE_PATH. "assets/snippets/cssjs/class.magic-min.php"); 
-	$minified = new Minifier();
-	$min = $minified->merge( MODX_BASE_PATH.$folder.'scripts.min.js', 'js', $filesForMin );
-	return '<script src="'.$modx->config['site_url'].$folder.'scripts.min.js?v='.substr(md5(max($v)),0,3).'"></script>';
-}else{
-	$links = '';
-	foreach ($filesArr as $key => $value) {
-		$links .= '<script src="'.$modx->config['site_url'].trim($value).'?v='.substr(md5($v[$key]),0,3).'"></script>';	
-	}	
-	return $links;
+    include_once(MODX_BASE_PATH. "assets/snippets/cssjs/class.magic-min.php"); 
+    $minified = new Minifier();
+    $min = $minified->merge(MODX_BASE_PATH.$folder.$filename.'.min.js', 'js', $filesForMin);
+    return '<script '.$filepre.' src="'.$modx->config['site_url'].$folder.$filename.'.min.js?v='.substr(md5(max($v)),0,3).'"></script>';
+} else {
+    $links = '';
+    foreach ($filesArr as $key => $value) {
+        $links .= '<script src="'.$modx->config['site_url'].trim($value).'?v='.substr(md5($v[$key]),0,3).'"></script>';    
+    }    
+    return $links;
 }
 ?>
